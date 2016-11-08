5a044b0793a4683b8071934d7cc87d1b3aef536c
tarunchadha <tarunchadha2040@gmail.com>
2016-11-08 20:07:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,10 +14,30 @@ button.onclick = function () {
           // Take some action
           
           if (request.status === 200){
+               // Create a request object  
+    
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readystate === XMLHttpRequest.DONE){
+          
+          // Take some action
+          
+          if (request.status === 200){
               
             var counter =  request.responseText;
             var span = document.getElementById('count');
             span.innerHTML = counter.toString();
+          }
+      }  
+        // Not done yet
+    };
+      // Make the request
+      
+      request.open('GET', 'http://tarunchadha.imad.hasura-app.io/submit-name?name='+ name, true);
+      request.send(null);
+            
           }
       }  
         // Not done yet
@@ -38,7 +58,8 @@ submit.onclick = function () {
     
     // Capture a list of names and render it as a list
     
-var names = ['name1', 'name2', 'name3', 'name4'];
+var names = request.responseText;
+names = JSON.parse(names);
 var list = '';
 for (var i=0; i< names.length; i++) {
     list += '<li>' + names[i] + '</li>';
